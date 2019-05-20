a48622caddf377fe44ffb44417bad356f5221b7a
David Gil Baeza <davidgil.univ@gmail.com>
2019-05-20 21:47:33 +0200

    Controladores y servicios de education record revisados

--- src/main/java/repositories/EducationRecordRepository.java
+++ src/main/java/repositories/EducationRecordRepository.java
@@ -2,11 +2,15 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.EducationRecord;
 
 @Repository
 public interface EducationRecordRepository extends JpaRepository<EducationRecord, Integer> {
+
+	@Query("select e from SocialWorker s join s.curriculum c join c.educationRecords e where s.id = ?1 and e.id = ?2")
+	EducationRecord getEducationReportOfSocialWorker(int socialWorkerId, int educationRecordId);
 
 }
