805407020d922879322229b7aa74f73ddea4797d
David Gil Baeza <davidgil.univ@gmail.com>
2019-05-21 10:07:47 +0200

    Arreglo populate y mas tests

--- src/test/java/services/CurriculumAndRecordsServicesTest.java
+++ src/test/java/services/CurriculumAndRecordsServicesTest.java
@@ -1,5 +1,8 @@
 package services;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
@@ -9,13 +12,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.EducationRecord;
 import domain.PersonalRecord;
 import utilities.AbstractTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/junit.xml" })
 @Transactional
-public class CurriculumAndRecordsServiceTest extends AbstractTest {
+public class CurriculumAndRecordsServicesTest extends AbstractTest {
 
 	@Autowired
 	private CurriculumService curriculumService;
@@ -25,6 +29,9 @@ public class CurriculumAndRecordsServiceTest extends AbstractTest {
 
 	@Autowired
 	private PersonalRecordService personalRecordService;
+
+	@Autowired
+	private EducationRecordService educationRecordService;
 
 	/**
 	 * R39. An actor who is authenticated as a SocialWorker must be able to:
@@ -237,6 +244,207 @@ public class CurriculumAndRecordsServiceTest extends AbstractTest {
 
 			this.personalRecordService.getPersonalRecordAsSocialWorker(personalRecordId);
 			this.socialWorkerService.updateCurriculum(personalRecord);
+
+			this.flushTransaction();
+
+			super.unauthenticate();
+		} catch (Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.rollbackTransaction();
+		}
+
+		super.checkExceptions(expected, caught);
+
+	}
+
+	/**
+	 * R39. An actor who is authenticated as a SocialWorker must be able to:
+	 *
+	 * 3. Manage his or her curriculum, which includes to create an education
+	 * record
+	 *
+	 * Ratio of data coverage: 5/8 = 62.5% - Access as a SocialWorker or not. -
+	 * SocialWorker has a curriculum or not - 5 attributes with restrictions
+	 *
+	 **/
+	@Test
+	public void driverCreateEducationRecord() {
+
+		Calendar c1 = Calendar.getInstance();
+		c1.add(Calendar.YEAR, -4);
+		Date date1 = c1.getTime();
+
+		Calendar c2 = Calendar.getInstance();
+		c2.add(Calendar.YEAR, -3);
+		Date date2 = c2.getTime();
+
+		Calendar c3 = Calendar.getInstance();
+		c3.add(Calendar.YEAR, 1);
+		Date date3 = c3.getTime();
+
+		Object testingData[][] = {
+
+				/**
+				 * POSITIVE TEST: SocialWorker is creating an education record
+				 * with correct information
+				 **/
+				{ "socialWorker1", "Title", "Institution", "http://www.attachment.com", date1, date2, null },
+				/**
+				 * NEGATIVE TEST: SocialWorker is creating an education record
+				 * but he or she has not a curriculum
+				 **/
+				{ "socialWorker3", "Title", "Institution", "http://www.attachment.com", date1, date2,
+						IllegalArgumentException.class },
+				/**
+				 * NEGATIVE TEST: Another user is trying to create an education
+				 * record
+				 **/
+				{ "salesMan1", "Title", "Institution", "http://www.attachment.com", date1, date2,
+						IllegalArgumentException.class },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to create an education
+				 * record record with incorrect dates
+				 **/
+				{ "socialWorker1", "Title", "Institution", "http://www.attachment.com", date3, date2,
+						ConstraintViolationException.class },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to create an education
+				 * record with a blank name title
+				 **/
+				{ "socialWorker1", "", "Institution", "http://www.attachment.com", date1, date2,
+						ConstraintViolationException.class } };
+
+		for (int i = 0; i < testingData.length; i++)
+			this.createEducationRecordTemplate((String) testingData[i][0], (String) testingData[i][1],
+					(String) testingData[i][2], (String) testingData[i][3], (Date) testingData[i][4],
+					(Date) testingData[i][5], (Class<?>) testingData[i][6]);
+
+	}
+
+	private void createEducationRecordTemplate(String socialWorker, String title, String institution, String attachment,
+			Date startDate, Date endDate, Class<?> expected) {
+
+		EducationRecord educationRecord = new EducationRecord();
+		educationRecord.setTitle(title);
+		educationRecord.setInstitution(institution);
+		educationRecord.setLink(attachment);
+		educationRecord.setStartDateStudy(startDate);
+		educationRecord.setEndDateStudy(endDate);
+
+		Class<?> caught = null;
+
+		try {
+			super.startTransaction();
+
+			super.authenticate(socialWorker);
+
+			this.curriculumService.addEducationRecord(educationRecord);
+
+			this.flushTransaction();
+
+			super.unauthenticate();
+		} catch (Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.rollbackTransaction();
+		}
+
+		super.checkExceptions(expected, caught);
+
+	}
+
+	/**
+	 * R39. An actor who is authenticated as a SocialWorker must be able to:
+	 *
+	 * 3. Manage his or her curriculum, which includes to update an education
+	 * record
+	 *
+	 * Ratio of data coverage: 6/9 = 66.67% - Access as a SocialWorker or not. -
+	 * SocialWorker has a curriculum or not - SocialWorker has the education
+	 * record or not - 5 attributes with restrictions
+	 *
+	 **/
+	@Test
+	public void driverUpdateEducationRecord() {
+
+		Calendar c1 = Calendar.getInstance();
+		c1.add(Calendar.YEAR, -4);
+		Date date1 = c1.getTime();
+
+		Calendar c2 = Calendar.getInstance();
+		c2.add(Calendar.YEAR, -3);
+		Date date2 = c2.getTime();
+
+		Calendar c3 = Calendar.getInstance();
+		c3.add(Calendar.YEAR, 1);
+		Date date3 = c3.getTime();
+
+		Object testingData[][] = {
+
+				/**
+				 * POSITIVE TEST: SocialWorker is updating an education record
+				 * with correct information
+				 **/
+				{ "socialWorker1", super.getEntityId("educationRecord1"), "Title", "Institution",
+						"http://www.attachment.com", date1, date2, null },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to update an education
+				 * record but he or she has not a curriculum
+				 **/
+				{ "socialWorker3", super.getEntityId("educationRecord1"), "Title", "Institution",
+						"http://www.attachment.com", date1, date2, IllegalArgumentException.class },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to update an education
+				 * record that does not belong to him or her
+				 **/
+				{ "socialWorker2", super.getEntityId("educationRecord1"), "Title", "Institution",
+						"http://www.attachment.com", date1, date2, IllegalArgumentException.class },
+				/**
+				 * NEGATIVE TEST: Another user is trying to update an education
+				 * record
+				 **/
+				{ "salesMan1", super.getEntityId("educationRecord1"), "Title", "Institution",
+						"http://www.attachment.com", date1, date2, IllegalArgumentException.class },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to update an education
+				 * record record with incorrect dates
+				 **/
+				{ "socialWorker1", super.getEntityId("educationRecord1"), "Title", "Institution",
+						"http://www.attachment.com", date3, date2, ConstraintViolationException.class },
+				/**
+				 * NEGATIVE TEST: SocialWorker is trying to update an education
+				 * record with a blank name title
+				 **/
+				{ "socialWorker1", super.getEntityId("educationRecord1"), "", "Institution",
+						"http://www.attachment.com", date1, date2, ConstraintViolationException.class } };
+
+		for (int i = 0; i < testingData.length; i++)
+			this.updateEducationRecordTemplate((String) testingData[i][0], (Integer) testingData[i][1],
+					(String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4],
+					(Date) testingData[i][5], (Date) testingData[i][6], (Class<?>) testingData[i][7]);
+
+	}
+
+	private void updateEducationRecordTemplate(String socialWorker, Integer educationRecordId, String title,
+			String institution, String attachment, Date startDate, Date endDate, Class<?> expected) {
+
+		EducationRecord educationRecord = this.educationRecordService.findOne(educationRecordId);
+		educationRecord.setTitle(title);
+		educationRecord.setInstitution(institution);
+		educationRecord.setLink(attachment);
+		educationRecord.setStartDateStudy(startDate);
+		educationRecord.setEndDateStudy(endDate);
+
+		Class<?> caught = null;
+
+		try {
+			super.startTransaction();
+
+			super.authenticate(socialWorker);
+
+			this.educationRecordService.getEducationRecordOfLoggedSocialWorker(educationRecordId);
+			this.curriculumService.updateEducationRecord(educationRecord);
 
 			this.flushTransaction();
 
