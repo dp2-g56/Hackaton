811a6bd43da3766226180a46aee77b0051f84c70
alegomcab1 <alegomcab1@gmail.com>
2019-05-21 11:51:35 +0200

    Commit encofrado

---
+++ src/main/java/forms/FormObjectVisitor.java
@@ -0,0 +1,148 @@
+
+package forms;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+public class FormObjectVisitor {
+
+	// USER ACCOUNT
+	private String	username;
+	private String	password;
+	private String	confirmPassword;
+
+	// ACTOR
+	private String	name;
+	private String	surname;
+	private String	middleName;
+	private String	photo;
+
+	// FORM
+	private Boolean	termsAndConditions;
+
+	// GUARD
+	private String	phoneNumber;
+	private String	emailEmergency;
+	private String	email;
+	private String	address;
+
+
+	public FormObjectVisitor() {
+		this.termsAndConditions = false;
+	}
+
+	@Size(min = 5, max = 32)
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Size(min = 5, max = 32)
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Size(min = 5, max = 32)
+	public String getConfirmPassword() {
+		return this.confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@NotBlank
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	@URL
+	public String getPhoto() {
+		return this.photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	@NotNull
+	public Boolean getTermsAndConditions() {
+		return this.termsAndConditions;
+	}
+
+	public void setTermsAndConditions(Boolean termsAndConditions) {
+		this.termsAndConditions = termsAndConditions;
+	}
+
+	@NotBlank
+	@Pattern(regexp = "[\\w.%-]+\\@[-.\\w]+\\.[A-Za-z]{2,4}|[\\w.%-]+\\<+[\\w.%-]+\\@[-.\\w]+\\.[A-Za-z]{2,4}|[\\w.%-]+\\<[\\w.%-]+\\@+\\>|[\\w.%-]+",
+		message = "Email doesn't follow the pattern, must be identifier@domain.asd or alias <identifier@domain.asd>")
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@NotBlank
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@NotBlank
+	@Pattern(regexp = "[\\w.%-]+\\@[-.\\w]+\\.[A-Za-z]{2,4}|[\\w.%-]+\\<+[\\w.%-]+\\@[-.\\w]+\\.[A-Za-z]{2,4}|[\\w.%-]+\\<[\\w.%-]+\\@+\\>|[\\w.%-]+",
+		message = "Email doesn't follow the pattern, must be identifier@domain.asd or alias <identifier@domain.asd>")
+	public String getEmailEmergency() {
+		return this.emailEmergency;
+	}
+
+	public void setEmailEmergency(String emailEmergency) {
+		this.emailEmergency = emailEmergency;
+	}
+
+	@NotBlank
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
