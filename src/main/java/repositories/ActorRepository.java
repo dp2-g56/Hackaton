f7823d67f1ffb8261f750c0badf71af9c6736ee8
alegomcab1 <alegomcab1@gmail.com>
2019-05-28 11:56:57 +0200

    Commit arreglo mensajeria

--- src/main/java/repositories/ActorRepository.java
+++ src/main/java/repositories/ActorRepository.java
@@ -30,12 +30,12 @@ public interface ActorRepository extends JpaRepository<Actor, Integer> {
 	@Query("select c.boxes from Actor c where c = ?1")
 	public List<Box> listOfBoxes(Actor actor);
 
-	@Query("select u.username from Prisoner p join p.userAccount u where p.crimeRate <= -0.5")
+	@Query("select u.username from Prisoner p join p.userAccount u where p.crimeRate <= -0.5 and p.isIsolated = false and p.freedom = false and p.isSuspect = false")
 	public List<Prisoner> getListOfPrisonersWithLowCrimRate();
 
 	@Query("select distinct u.username from Visitor v join v.userAccount u join v.visits w where w.prisoner = ?1")
 	public List<Visitor> getVisitorsWithVisitsOfPrisoner(Prisoner prisoner);
 
-	@Query("select u.username from Actor a join a.userAccount u where a not in (select p from Prisoner p join p.userAccount u where u.isNotLocked = false or p.freedom = true or p.isIsolated = true)")
+	@Query("select u.username from Actor a join a.userAccount u where a not in (select p from Prisoner p join p.userAccount u where u.isNotLocked = false or p.freedom = true or p.isIsolated = true or p.crimeRate > 0.5)")
 	public List<String> getUsernamesOfActorsAndGoodPrisoners();
 }
