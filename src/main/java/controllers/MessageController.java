52eb5c5ccb692383bab2ac495261cb133f9e4596
alegomcab1 <alegomcab1@gmail.com>
2019-05-29 14:50:46 +0200

    Commit messages

--- src/main/java/controllers/MessageController.java
+++ src/main/java/controllers/MessageController.java
@@ -178,13 +178,18 @@ public class MessageController extends AbstractController {
 
 	//Save
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
-	public ModelAndView delete(@RequestParam int rowId, @RequestParam int boxId) {
+	public ModelAndView delete(@RequestParam(required = false) String rowId, @RequestParam(required = false) String boxId) {
 		try {
 			this.actorService.loggedAsActor();
+			Assert.isTrue(StringUtils.isNumeric(rowId));
+			int messageIdInt = Integer.parseInt(rowId);
 
-			Message message = this.messageService.findOne(rowId);
+			Assert.isTrue(StringUtils.isNumeric(boxId));
+			int boxIdInt = Integer.parseInt(boxId);
+
+			Message message = this.messageService.findOne(messageIdInt);
 			UserAccount userAccount = LoginService.getPrincipal();
-			Box currentBox = this.boxService.findOne(boxId);
+			Box currentBox = this.boxService.findOne(boxIdInt);
 			Actor a = this.actorService.getActorByUsername(userAccount.getUsername());
 			ModelAndView result;
 
@@ -194,11 +199,13 @@ public class MessageController extends AbstractController {
 				return new ModelAndView("redirect:/box/actor/list.do");
 
 			try {
-				if (currentBox.equals(trashBox))
+				if (currentBox.equals(trashBox)) {
 					this.messageService.deleteMessageFinal(message);
-				else
+					this.messageService.flush();
+				} else {
 					this.messageService.deleteMessageToTrashBox(message);
-
+					this.messageService.flush();
+				}
 				result = new ModelAndView("redirect:/box/actor/list.do");
 			} catch (Throwable oops) {
 				result = this.createEditModelAndView(message, "message.commit.error");
@@ -209,7 +216,6 @@ public class MessageController extends AbstractController {
 			return new ModelAndView("redirect:/list.do");
 		}
 	}
-
 	//Create
 	@RequestMapping(value = "/createmove", method = RequestMethod.GET)
 	public ModelAndView createMove() {
@@ -247,10 +253,10 @@ public class MessageController extends AbstractController {
 
 			try {
 				this.messageService.updateMessage(message, box);
+				this.messageService.flush();
 				result = new ModelAndView("redirect:/box/actor/list.do");
 			} catch (Throwable oops) {
 				result = this.createEditModelAndViewMove(message, "message.commit.error");
-
 			}
 			return result;
 		} catch (Throwable oops2) {
@@ -276,10 +282,10 @@ public class MessageController extends AbstractController {
 
 			try {
 				this.messageService.copyMessage(message, box);
+				this.messageService.flush();
 				result = new ModelAndView("redirect:/box/actor/list.do");
 			} catch (Throwable oops) {
 				result = this.createEditModelAndViewMove(message, "message.commit.error");
-
 			}
 			return result;
 		} catch (Throwable oops2) {
